4c87b2d7cac877fd1efe91bda8a60f658ffe712e
inshining <inshining.kim@gmail.com>
2023-03-23 00:28:13 +0900

    ➕[ADD] : jumjaro 파일 실행

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from backend.model.braille import BrailleBody, BrailleResponse
-from backend.service.traslate import translate_braille_to_korean
+from backend.service.traslate import translate_braille_to_korean, translate_korean_to_braille, convert_str_to_unicodes
 
 app = FastAPI()
 
@@ -12,13 +12,23 @@ async def root():
 
 
 @app.post("/translation/braille")
-async def say_hello(braille_instance: BrailleBody) -> BrailleResponse:
-    raw_braille = braille_instance.braille
-    kor = translate_braille_to_korean(raw_braille)[0]
+async def translate_2_korean(braille_instance: BrailleBody) -> BrailleResponse:
+    braille = braille_instance.braille
+    kor = translate_braille_to_korean(braille)[0]
 
-    braille_unicodes = [ord(bra_unicode) for bra_unicode in raw_braille]
+    braille_unicodes = convert_str_to_unicodes(braille)
     response = BrailleResponse(str=kor, braille=braille_unicodes)
 
     return response
 
+@app.get("/translation")
+async def translate_2_braille(kor: str | None = None):
+    if kor:
+        response = []
+        for letter in kor:
+            brailles = translate_korean_to_braille(letter)
+            unicodes = convert_str_to_unicodes(brailles)
+            response.append(BrailleResponse(str=letter, braille=unicodes))
+        return response
+    return {"is_success": False, "data": "번역되지 않았습니다."}
 
--- backend/service/traslate.py
+++ backend/service/traslate.py
@@ -1,5 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from pythonnet import load
+load("coreclr")
 
+import clr
+
+clr.AddReference("./backend/service/Jumjaro")
+
+from Jumjaro import Jumjaro
 def translate_braille_to_korean(braille_pattern: str):
     # get tokenizer, config and pretrained weights
     tokenizer = AutoTokenizer.from_pretrained("snoop2head/KoBrailleT5-small-v1")
@@ -14,3 +21,10 @@ def translate_braille_to_korean(braille_pattern: str):
     outputs = model.generate(input_ids)
     korean_output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     return korean_output
+
+
+def translate_korean_to_braille(korean: str):
+    return Jumjaro().ToJumja(korean)
+
+def convert_str_to_unicodes(value: str) -> list[int]:
+    return [ord(char) for char in value]
